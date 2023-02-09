64124281d38cf44f8f666905d8954258d6c76313
MitchellV3 <mitchellvollmer@shaw.ca>
2023-02-09 00:00:13 -0700

    Set up login page

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -57,7 +58,20 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+          HttpSession session = request.getSession();
+  
+            String logout = request.getParameter("logout");
+            if (logout != null) {
+              session.invalidate();
+              request.setAttribute("message", "You have successfully logged out.");
+              getServletContext().getRequestDispatcher("/WEB-INF/login.jsp")
+                .forward(request, response);
+              return;
+            }
+
+           getServletContext().getRequestDispatcher("/WEB-INF/login.jsp")
+                .forward(request, response);
+          
     }
 
     /**
--- web/WEB-INF/login.jsp
+++ web/WEB-INF/login.jsp
@@ -9,9 +9,17 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Login</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Login</h1>
+        <form method="post" action="note">
+          <p>Username:</p>
+          <input type="text" name="username"><br>
+          <p>Password:</p>
+          <input type="text" name="password"><br>          
+          <br>
+          <input type="submit" value="Log in">
+        </form>
     </body>
 </html>
